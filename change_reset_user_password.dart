29caa708b325b6fe5a3ecdd518d0ee648096ddc0
Binho.Dev <58571495+binhodev@users.noreply.github.com>
2021-01-11 20:11:42 -0300

    Create change_reset_user_password.dart

---
+++ change_reset_user_password.dart
@@ -0,0 +1,19 @@
+final FirebaseAuth _auth = FirebaseAuth.instance;
+
+Future<void> resetPassword() async {
+  try {
+    await _auth.sendPasswordResetEmail(email: 'usermail@user.com');
+  } on FirebaseAuthException catch (e) {
+    print(e.code);
+  }
+}
+
+Future<void> changePassword({User user, String password}) async {
+  final User userCredential = user ?? _auth.currentUser;
+
+  await userCredential.updatePassword(password).then((_) {
+    print('Sucesso ao trocar a senha');
+  }).catchError((error) {
+    print(error);
+  });
+}

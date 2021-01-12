f95c0cf1a05b072d634e006b69ea5bc52c7a6cf8
Binho.dev <binho.dev@gmail.com>
2021-01-11 23:34:37 -0300

    Carregar as informações de perfil (exemplo)

--- firebase_auth_users_operations.dart
+++ firebase_auth_users_operations.dart
@@ -4,16 +4,32 @@ class Authentication {
   
   final FirebaseAuth _auth = FirebaseAuth.instance;
 
+  //Criando um usuário com email e senha
   Future<void> createUser() async {
     try {
       final UserCredential userCredential = 
         await _auth.createUserWithEmailAndPassword(email: 'usermail@user.com', password: '12345678');
       print('New user id: ${userCredential.user.uid}');
+
+      //Se deseja carregar as informações do usuário...
+      //logo após a criação da conta.
+      loadingCurrentUser();
     } on FirebaseAuthException catch (e) {
       print(e.code); // Preferencial o uso do debugPrint.
     }
   }
 
+  //Carregando o usuário atual
+  Future<void> loadingCurrentUser({User user}) async {
+    final User userCredential = user ?? _auth.currentUser;
+    if (userCredential != null) {
+      //Você pode inserir outras funções aqui, por exemplo...
+      //carregar informações do perfil do usuário que acabou de entrar.
+    }
+    print('Usuário atual logado: ${userCredential?.email ?? 'Nenhum usuário logado'}');
+  }
+
+  //Redefinição da senha do usuário
   Future<void> resetUserPassword() async {
     try {
       await _auth.sendPasswordResetEmail(email: 'usermail@user.com');
@@ -22,6 +38,7 @@ class Authentication {
     }
   }
 
+  //Alterando a senha do usuário
   Future<void> changeUserPassword({User user, String password}) async {
     final User userCredential = user ?? _auth.currentUser;
 
